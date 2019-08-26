202bbe9bb40f308ca172e75becd7acd46f08807f
Lukas Elmer <lukas.elmer@gmail.com>
2019-08-26 15:03:25 +0200

    logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,8 @@ module.exports = {
         name: `ese-website`,
         short_name: `ese`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2196f3`,
+        theme_color: `#2196f3`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },

9e16b11afcf645b8badf2657c91e34f07b703439
Lukas Elmer <lukas.elmer@gmail.com>
2020-06-22 15:25:39 +0200

    Fix deprecation issues

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,9 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Image from "../components/image"
 
 export default class Cv extends React.Component {
   componentDidMount() {
